2327132b1535351d311c04cd954edae7a51921df
Amirah Oxendine <amirahoxendine@ac.c4q.nyc>
2018-03-20 01:30:46 -0400

    added create event : send event to db feature

--- app/src/main/java/com/example/c4q/capstone/userinterface/events/CreateEventPresenter.java
+++ app/src/main/java/com/example/c4q/capstone/userinterface/events/CreateEventPresenter.java
@@ -6,10 +6,22 @@ import android.widget.DatePicker;
 import android.widget.TimePicker;
 
 import com.example.c4q.capstone.database.model.events.Events;
+import com.example.c4q.capstone.database.model.publicuserdata.PublicUser;
 import com.example.c4q.capstone.userinterface.events.eventfragments.CreateEventFragment;
+import com.firebase.ui.auth.data.model.User;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by amirahoxendine on 3/19/18.
@@ -19,19 +31,69 @@ import java.util.List;
 public class CreateEventPresenter {
     private static Events newEvent;
     private static List<String> invitedGuests = new ArrayList<>();
-    private boolean eventNameSet, eventDateSet, eventTimeSet, eventGuestsSet, eventOrganizerSet;
+    private boolean eventNameSet, eventDateSet, eventTimeSet, eventGuestsSet;
     public String dateOfEvent;
     public String timeOfEvent;
     public String dateTime = "";
     private static String TAG = "CREATE_EVENT_PRES: ";
 
+    private FirebaseDatabase mFirebaseDatabase;
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private DatabaseReference myRef;
+    private FirebaseUser user;
+    private static final String PUBLIC_USER = "public_user";
+    private String userID;
+    String firstName;
+    String lastName;
+
+    DatabaseReference database = FirebaseDatabase.getInstance().getReference();
+    DatabaseReference ref = database.child(PUBLIC_USER);
+
     public CreateEventPresenter(){
         newEvent = new Events();
+        mAuth = FirebaseAuth.getInstance();
+        mFirebaseDatabase = FirebaseDatabase.getInstance();
+        myRef = mFirebaseDatabase.getReference();
+        user = mAuth.getCurrentUser();
+        userID = user.getUid();
+        setEventOrganizer();
     }
 
     public void sendEventToFB(){
         //TODO logic to send newEvent to firebase
+        // Create new post at /user-posts/$userid/$postid and at
+        // /posts/$postid simultaneously
+        Log.d(TAG, "create event: eventSent to firebase" + newEvent.getEvent_name());
+        String key = myRef.child("events").push().getKey();
+        Log.d(TAG, "create event: push key " + key);
+        myRef.child("events").child(key).setValue(newEvent);
+        Log.d(TAG, "create event: set value: " + newEvent.getEvent_name());
+    }
 
+    public void getUserData(){
+        ValueEventListener postListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // Get Post object and use the values to update the UI
+                PublicUser user = dataSnapshot.getValue(PublicUser.class);
+                firstName = user.getFirst_name();
+                lastName = user.getLast_name();
+                // ...
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                // Getting Post failed, log a message
+                Log.w(TAG, "loadPost:onCancelled", databaseError.toException());
+                // ...
+            }
+        };
+        myRef.addValueEventListener(postListener);
+    }
+
+    public void setEventOrganizer(){
+        newEvent.setEvent_organizer(userID);
     }
 
     public void setEventName(String eventName){
@@ -73,11 +135,6 @@ public class CreateEventPresenter {
         dateTime = sb.toString();
     }
 
-    public void setEventOrganizer(String eventOrganizerId){
-        newEvent.setEvent_organizer(eventOrganizerId);
-        eventOrganizerSet = true;
-    }
-
     public void setEventGuests(){
         newEvent.setInvited_guests(invitedGuests);
         if(invitedGuests.size() == 0){
@@ -91,7 +148,14 @@ public class CreateEventPresenter {
     }
 
     public boolean validateEvent(){
-        return eventGuestsSet && eventOrganizerSet && eventTimeSet &&eventNameSet && eventDateSet;
+        boolean validEvent = false;
+        if (eventTimeSet &&eventNameSet && eventDateSet){
+            Log.d(TAG, "create event: event valid");
+            validEvent = true;
+        } else {
+            Log.d(TAG, "create event: event not valid");
+        }
+        return validEvent;
     }
 
     public void inviteGuest(String guestId){
