8147c807cce1d86807bdf7776c5eec579170c364
Tatiana <tatianamoronta@ac.c4q.nyc>
2018-03-21 09:39:52 -0400

    changed to checkboxes

--- app/src/main/java/com/example/c4q/capstone/userinterface/user/userprofilefragments/PreferencesFragment.java
+++ app/src/main/java/com/example/c4q/capstone/userinterface/user/userprofilefragments/PreferencesFragment.java
@@ -3,6 +3,7 @@ package com.example.c4q.capstone.userinterface.user.userprofilefragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,7 +15,10 @@ import com.example.c4q.capstone.R;
  */
 public class PreferencesFragment extends Fragment {
     private View view;
-
+    private View clubRadio;
+    private View loungeRadio;
+    private View beerRadio;
+    private View karaokeRadio;
 
     public PreferencesFragment() {
         // Required empty public constructor
@@ -26,6 +30,13 @@ public class PreferencesFragment extends Fragment {
                              Bundle savedInstanceState) {
 
         view = inflater.inflate(R.layout.fragment_preferences, container, false);
+
+        clubRadio = view.findViewById(R.id.club_pref);
+        loungeRadio = view.findViewById(R.id.lounge_pref);
+        beerRadio = view.findViewById(R.id.beer_pref);
+        karaokeRadio = view.findViewById(R.id.karaoke_pref);
+
+
 
 
 
