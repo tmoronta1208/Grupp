37e58a3b3bfa2bfcc36d436754e9ac80f0362496
Ashley Joachim <ashleyjoachim@ac.c4q.nyc>
2018-03-21 19:53:42 -0400

    updating the send request

--- app/src/main/java/com/example/c4q/capstone/userinterface/user/search/UserSearchActivity.java
+++ app/src/main/java/com/example/c4q/capstone/userinterface/user/search/UserSearchActivity.java
@@ -6,18 +6,29 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Toast;
 
 import com.example.c4q.capstone.R;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import static com.example.c4q.capstone.utils.Constants.NOTIFICATIONS;
 import static com.example.c4q.capstone.utils.Constants.USER_SEARCH;
 
 public class UserSearchActivity extends AppCompatActivity {
     private RecyclerView searchResultsList;
-    private DatabaseReference searchUserDatabase;
+    private DatabaseReference rootRef, newNotification, searchUserDatabase, friendReqDatabase, friendDatabase, notificationDatabase, usersDatabase;
     private LinearLayoutManager linearLayoutManager;
+    FirebaseUser currentUser;
+    String currentState;
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -31,6 +42,9 @@ public class UserSearchActivity extends AppCompatActivity {
         searchResultsList = findViewById(R.id.search_users_rv);
         searchResultsList.setHasFixedSize(true);
         searchResultsList.setLayoutManager(linearLayoutManager);
+
+        currentState = "not_friends";
+
     }
 
     @Override
@@ -51,7 +65,14 @@ public class UserSearchActivity extends AppCompatActivity {
                     protected void populateViewHolder(UserSearchViewHolder viewHolder, User model, int position) {
                         viewHolder.setEmail(model.getEmail());
 
-                        final String user_id = getRef(position).getKey();
+                       final String user_id = getRef(position).getKey();
+
+                        viewHolder.view.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                sendRequest(user_id);
+                            }
+                        });
 
 
                         /**
@@ -66,5 +87,46 @@ public class UserSearchActivity extends AppCompatActivity {
                 };
 
         searchResultsList.setAdapter(firebaseRecyclerAdapter);
+    }
+
+    private void sendRequest(String user_id) {
+        if (currentState.equals("not_friends")) {
+
+            newNotification = rootRef.child(NOTIFICATIONS).child(user_id).push();
+            String newNotificationId = newNotification.getKey();
+
+            HashMap<String, String> notificationData = new HashMap<>();
+            notificationData.put("from", currentUser.getUid());
+            notificationData.put("type", "request");
+
+            Map requestMap = new HashMap();
+            requestMap.put("Friend_req/" + currentUser.getUid() + "/" + user_id + "/request_type", "sent");
+            requestMap.put("Friend_req/" + user_id + "/" + currentUser.getUid() + "/request_type", "received");
+            requestMap.put("notifications/" + user_id + "/" + newNotificationId, notificationData);
+
+            rootRef.updateChildren(requestMap, new DatabaseReference.CompletionListener() {
+                @Override
+                public void onComplete(DatabaseError databaseError, DatabaseReference databaseReference) {
+
+                    if (databaseError != null) {
+
+                        Toast.makeText(UserSearchActivity.this, "There was some error in sending request", Toast.LENGTH_SHORT).show();
+
+                    } else {
+
+                        currentState = "req_sent";
+//                        profileSendReqBtn.setText("Cancel Friend Request");
+
+                    }
+
+//                    profileSendReqBtn.setEnabled(true);
+
+
+                }
+            });
+
+        }
+
+
     }
 }
