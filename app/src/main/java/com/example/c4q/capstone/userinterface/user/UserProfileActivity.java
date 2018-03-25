b44cbe88a333f1d011fe1e28db9e31a12a9086f5
Melina Gonzalez <melinag1992@gmaill.com>
2018-03-24 20:28:17 -0400

    Create event button

--- app/src/main/java/com/example/c4q/capstone/userinterface/user/UserProfileActivity.java
+++ app/src/main/java/com/example/c4q/capstone/userinterface/user/UserProfileActivity.java
@@ -5,31 +5,25 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
-import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.app.SupportActivity;
 
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.PopupMenu;
 import android.widget.TextView;
 
 import com.example.c4q.capstone.R;
 import com.example.c4q.capstone.userinterface.CurrentUser;
-import com.example.c4q.capstone.userinterface.navdrawer.NavDrawerPresenter;
+import com.example.c4q.capstone.userinterface.events.createevent.CreateEventActivity;
 import com.example.c4q.capstone.userinterface.user.search.UserSearchActivity;
-import com.example.c4q.capstone.userinterface.user.userprofilefragments.MainUserProfileFragment;
 import com.example.c4q.capstone.userinterface.user.userprofilefragments.UPEventsFragment;
 import com.example.c4q.capstone.userinterface.user.userprofilefragments.UPGroupFragment;
 
@@ -138,6 +132,9 @@ public class UserProfileActivity extends AppCompatActivity{
                 startActivity(new Intent(UserProfileActivity.this, UserSearchActivity.class));
                 break;
             case R.id.add_group_menu_item:
+
+                startActivity(new Intent(UserProfileActivity.this, CreateEventActivity.class));
+
                 //TODO
                 break;
         }
