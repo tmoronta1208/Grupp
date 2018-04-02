6ebdf5527c0fae90a64fbaa477b78e55f5fd013b
Tatiana <tatianamoronta@ac.c4q.nyc>
2018-04-02 16:55:20 -0400

    fixed db additions and fixed layout for creating group

--- app/src/main/java/com/example/c4q/capstone/userinterface/user/userprofilefragments/UPGroupDisplayFragment.java
+++ app/src/main/java/com/example/c4q/capstone/userinterface/user/userprofilefragments/UPGroupDisplayFragment.java
@@ -3,19 +3,27 @@ package com.example.c4q.capstone.userinterface.user.userprofilefragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.c4q.capstone.R;
+import com.example.c4q.capstone.userinterface.user.userprofilefragments.userprofilecontroller.GroupsAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class UPGroupDisplayFragment extends Fragment {
 
+    RecyclerView recyclerView;
+    GroupsAdapter groupsAdapter;
+    List<Integer> numbers = new ArrayList<>();
     View rootView;
-
 
 
     public UPGroupDisplayFragment() {
@@ -29,12 +37,19 @@ public class UPGroupDisplayFragment extends Fragment {
         // Inflate the layout for this fragment
         rootView = inflater.inflate(R.layout.fragment_upgroup_display, container, false);
 
+        for (int i = 0; i < 6; i++) {
+
+            numbers.add(i);
+
+        }
+
+        recyclerView = rootView.findViewById(R.id.grupp_display_member_rv);
 
 
-
-
-
-
+        groupsAdapter = new GroupsAdapter(getContext(), numbers);
+        recyclerView.setLayoutManager(new GridLayoutManager(rootView.getContext(), 3));
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setAdapter(groupsAdapter);
 
 
         return rootView;
